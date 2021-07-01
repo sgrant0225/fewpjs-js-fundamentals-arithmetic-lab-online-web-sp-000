462c01ad645d2078cc3458a9340fbb8149a51b92
github username <sharnellgrant@gmail.com>
2021-07-01 11:04:00 -0400

    commit tree 6b60e0c9d800c21f3b6567425267ad12280b3d88

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+//spec 1
+let num1 = 2;
+let num2 = 31; 
+const multiply = num1 * num2
+//spec 2
+let random = Math.random();
+//spec 3
+const mod = 16 % 6
+//sepc 4
+const max = Math.max(2, 4, 6, 10, 20)
+
+
